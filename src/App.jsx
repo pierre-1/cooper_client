da55d5b222ba8050cd2ec328cf2a9242cbf5d6fc
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-26 17:38:18 +0100

    adding axios and authenticate function to connect with the api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from "./components/InputFields";
 import LoginForm from "./components/LoginForm";
+import { authenticate } from './modules/auth'
 
 class App extends Component {
   state = {
