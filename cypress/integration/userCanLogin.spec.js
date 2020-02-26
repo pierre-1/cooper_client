577ba149af40994d94edc9df9fdd79b41040822c
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-26 15:24:35 +0100

    Starting with acceptance tests for Login feature

---
+++ cypress/integration/userCanLogin.spec.js
@@ -0,0 +1,17 @@
+
+describe('User authenticates', () => {
+    beforeEach(() => {
+        cy.visit("/");
+    });
+
+    it ('successfully with valid credentials', () => {
+        cy.get('#login').click();
+        cy.get('#login-form').within(() => {
+            cy.get('#email').type('user@mail.com');
+            cy.get('#password').type('password');
+            cy.get('button').contains('Submit').click()
+        });
+        cy.get('#message').should('contain', 'Hi user@mail.com');
+    });
+    
+})
