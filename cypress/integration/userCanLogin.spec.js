49ba83a6e8ab4cccd2d6801ebb8d401c39cb2728
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-26 17:17:15 +0100

    Adds LoginForm logic to App component and LoginForm

--- cypress/integration/userCanLogin.spec.js
+++ cypress/integration/userCanLogin.spec.js
@@ -4,7 +4,7 @@ describe('User authenticates', () => {
         cy.visit("/");
     });
 
-    it ('successfully with valid credentials', () => {
+    it('successfully with valid credentials', () => {
         cy.get('#login').click();
         cy.get('#login-form').within(() => {
             cy.get('#email').type('user@mail.com');
@@ -13,5 +13,14 @@ describe('User authenticates', () => {
         });
         cy.get('#message').should('contain', 'Hi user@mail.com');
     });
-    
-})
+
+    it("unsuccessfully with invalid credentials", () => {
+        cy.get("#login").click();
+        cy.get('#login-form').within(() => {
+            cy.get('#email').type('user@mail.com');
+            cy.get('#password').type('wrongpassword');
+            cy.get('button').contains('Submit').click()
+        });
+        cy.get("#message").should("contain", "Invalid login credentials. Please try again.");
+    });
+});
