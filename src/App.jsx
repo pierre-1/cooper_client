68453d1096037a71af2336c75e3927738eded65d
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-27 10:23:31 +0100

    the tests are not passing due to the login button not showing anymore

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from "./components/InputFields";
 import LoginForm from "./components/LoginForm";
-import { authenticate } from './modules/auth'
+import { authenticate } from "./modules/auth";
 
 class App extends Component {
   state = {
@@ -32,16 +32,31 @@ class App extends Component {
   };
 
   render() {
-    const renderLogin = this.state.renderLoginForm ? (
-      <LoginForm submitFormHandler={this.onLogin} />
-    ) : (
-      <button
-      id="login"
-      onClick={() => this.setState({ renderLoginForm: true })}
-      >
-        Login
-      </button>
-    );
+    const { renderLoginForm, authenticated, message } = this.state;
+    let renderLogin;
+    switch (true) {
+      case renderLoginForm && !authenticated:
+        renderLogin = <LoginForm submitFormHandler={this.onLogin} />;
+        break;
+      case !renderLoginForm && authenticated:
+        renderLogin = (
+          <>
+            <button
+              id="login"
+              onClick={() => this.setState({ renderLoginForm: true })}
+            >
+              Login
+            </button>
+            <p id='message'>{message}</p>
+          </>
+        );
+        break;
+      case authenticated:
+        renderLogin = (
+          <p id='message'>Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+        );
+        break;
+    }
     return (
       <>
         <InputFields onChangeHandler={this.onChangeHandler} />
