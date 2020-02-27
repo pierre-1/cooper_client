7f4f12269b28d6cd44452ff25a5c9bd5bdcf2910
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-27 12:09:01 +0100

    Adding specs and starting to add functionality to save the data

--- cypress/integration/userCanLogin.feature.js
+++ cypress/integration/userCanLogin.feature.js
@@ -1,10 +1,19 @@
 
-describe('User authenticates', () => {
+describe('User can log in', () => {
     beforeEach(() => {
+        cy.server();
         cy.visit("/");
     });
 
-    it('successfully with valid credentials', () => {
+    it('successfully', () => {
+        cy.route({
+            method: 'POST',
+            url: "http://localhost:3000/api/v1/auth/sign_in",
+            response: 'fixture:login.json',
+            headers: {
+                uid: 'user@mail.com'
+            }
+        });
         cy.get('#login').click();
         cy.get('#login-form').within(() => {
             cy.get('#email').type('user@mail.com');
@@ -14,7 +23,16 @@ describe('User authenticates', () => {
         cy.get('#message').should('contain', 'Hi user@mail.com');
     });
 
-    it("unsuccessfully with invalid credentials", () => {
+    it("with invalid credentials", () => {
+        cy.route({
+            method: 'POST',
+            url:  "http://localhost:3000/api/v1/auth/sign_in",
+            status: "401",
+            response: {
+                errors: ['Invalid login credentials. Please try again.'],
+                success: false
+            }
+        });
         cy.get("#login").click();
         cy.get('#login-form').within(() => {
             cy.get('#email').type('user@mail.com');
